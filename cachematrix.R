7473d5a7062e99a12ca3f3c275efb0a073d329b5
Dariusz Szuksztul <dszuksztul@atlassian.com>
2015-02-18 23:48:59 +0100

    implement cached matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## create 'object' that hold matrix and cached value associated with this matrix
+## (by name convetion this value is an inversion of matrix)
+## setting new matrix on that object, also resets associated value
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(newInv) inv <<- newInv
+  getinv <- function() inv
+  
+  list( set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## calculates inversion of matrix stored in 'makeCacheMatrix' object
+## if object contains associatedValue returns it as inversion
+## otherwise calculates inversion and additionally caches it in given object
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if ( !is.null(inv)) {
+    return(inv)
+  }
+  matrix = x$get()
+  inv <- solve(matrix)
+  x$setinv(inv)
+  
+  inv
 }
